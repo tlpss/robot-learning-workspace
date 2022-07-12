ee2293f2a16fd5580dc5cd3847d477c702d7cb0a
tlips <thomas.lips@ugent.be>
2022-07-12 20:05:33 +0200

    IK with IKFast as the Pybullet IK suffers from joint flips..

---
+++ ur-sim/ur_sim/test_ikfast.py
@@ -0,0 +1,21 @@
+from ur_ikfast.ur_ikfast import ur_kinematics
+import numpy as np
+from scipy.spatial.transform import Rotation
+if __name__ == "__main__":
+    ur3e_arm = ur_kinematics.URKinematics('ur3e')
+
+    """
+    IKFast seems to struggle with quaternions along axis (wich leads to zeros..)
+    also note that the order for the wrapper is w,x,y,z!
+    """
+    joint_angles = [-1.6, -1.6, 1.6, -1.6, -1.6, 0.]  # in radians
+    print("joint angles", joint_angles)
+    pose_quat = ur3e_arm.forward(joint_angles)
+    print("forward() quaternion \n", pose_quat)
+
+    # print("inverse() all", ur3e_arm.inverse(pose_quat, True))
+    pose_quat =  [-0.2,-0.2,0.2,  0.0,1.0,0.0,0]
+    print("inverse() one from quat", ur3e_arm.inverse(pose_quat, False, q_guess=joint_angles))
+
+    pose_quat[4:] += np.random.randn(3) * 0.01
+    print("inverse() one from noisy quat", ur3e_arm.inverse(pose_quat, False, q_guess=joint_angles))
