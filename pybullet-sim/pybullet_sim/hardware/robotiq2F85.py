dcfc6d85722bd9dc5494e623111138c83babd9ee
tlips <thomas.lips@ugent.be>
2022-09-02 13:26:29 +0200

    add WSG50 as the 2F85 is too unstable..

--- pybullet-sim/pybullet_sim/hardware/robotiq2F85.py
+++ pybullet-sim/pybullet_sim/hardware/robotiq2F85.py
@@ -33,7 +33,7 @@ class Gripper():
     def close_gripper(self,max_force: int = 100):
         self.movej(Gripper.closed_relative_position, max_force)
 
-    def movej(self, target_relative_position:float, max_force: int = 100, max_steps:int = 500):
+    def movej(self, target_relative_position:float, max_force: int = 100, max_steps:int = 250):
         # bookkeeping
         self.target_relative_position = target_relative_position
 
@@ -46,7 +46,7 @@ class Gripper():
             p.stepSimulation()
             if self.simulate_real_time:
                 time.sleep(1.0 / 240)
-        print(f"Warning: movej exceeded {max_steps} simulation steps for {self.__class__}. Skipping.")
+        logging.debug(f"Warning: movej exceeded {max_steps} simulation steps for {self.__class__}. Skipping.")
         
     
     def is_object_grasped(self):
@@ -63,15 +63,52 @@ class Gripper():
         raise NotImplementedError
 
 
+class WSG50(Gripper):
 
+    tcp_offset = np.array([0.0,0.0,0.153])
+    closed_position = 0.050
+    open_position = 0.0
+    def __init__(self, simulate_realtime: bool = True) -> None:
+        gripper_id = p.loadURDF(str(get_asset_root_folder() / "wsg50"/ "wsg50.urdf"))
+        super().__init__(gripper_id, WSG50.tcp_offset, simulate_realtime)
+        self._create_constraints()
+
+    def _create_constraints(self):
+        c = p.createConstraint(self.gripper_id, 0,
+                            self.gripper_id, 2,
+                            jointType=p.JOINT_GEAR,
+                            jointAxis=[1,0, 0],
+                            parentFramePosition=[0, 0, 0],
+                            childFramePosition=[0, 0, 0])
+        p.changeConstraint(c, gearRatio=-1, maxForce=50, erp=0.1)  # Note: the mysterious `erp` is of EXTREME importance
+    
+    def _set_joint_targets(self, target_relative_position ,max_force):
+        open_angle = self._relative_position_to_joint_opening(target_relative_position)
+        for id in [0,2]:
+            p.setJointMotorControl2(self.gripper_id, id,p.POSITION_CONTROL,targetPosition=open_angle,force=max_force, maxVelocity=0.1)
+
+    def get_relative_position(self):
+        joint_config = p.getJointState(self.gripper_id, 0)[0]
+        return self._joint_opening_to_relative_position(joint_config)
+
+    def attach_with_constraint_to_robot(self, robot_id, robot_link_id):
+        p.createConstraint(robot_id,robot_link_id, self.gripper_id, -1, p.JOINT_FIXED, [0, 0, 0.0], [0.0, 0.0, 0], [0, 0,-0.005])
+    @staticmethod
+    def _joint_opening_to_relative_position(abs_position:float) -> float:
+        rel_position = (abs_position - WSG50.open_position) / (WSG50.closed_position-WSG50.open_position)
+        return rel_position
+
+    @staticmethod
+    def _relative_position_to_joint_opening(relative_position: float) -> float:
+        abs_position = WSG50.open_position + (WSG50.closed_position - WSG50.open_position) * relative_position
+        return abs_position
 class Robotiq2F85(Gripper):
     """
     the Robotiq grippers are a pain to simulate as they have a closed chain due to their parallel inner and outer knuckles.
     Actuating the 6 joints seperately is not recommended as the joints would close faster/slower, resulting in unrealistic grasping. 
-    In fact all joints on each finger (3/finger) should mimic each other, and so do the 2 fingers, however this resulted in physics instabilities so I 
-    hacked until something worked. This is specified in URDF but requires manual constraints in pybullet.
+    In fact all joints on each finger (3/finger) should mimic each other, and so do the 2 fingers.
 
-    DUE TO WRONG FINGER TIP WEIGHTS, the physics were very unstable. After fixing this, the constraints worked like a charm. 
+    The resulting physics are rather unstable however.. Grasping an object for example is very tricky..
     
     """
     open_position = 0.000
@@ -132,34 +169,32 @@ if __name__ == "__main__":
     physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
     p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
     p.setGravity(0, 0, -10)
-    tableId = p.loadURDF(str(get_asset_root_folder() / "ur3e_workspace" / "workspace.urdf"), [0, -0.3, -0.01])
+    tableId = p.loadURDF(str(get_asset_root_folder() / "ur3e_workspace" / "workspace.urdf"), [0, -0.3, -0.001])
 
     target = p.getDebugVisualizerCamera()[11]
     p.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW, 1)
 
     p.resetDebugVisualizerCamera(cameraDistance=1.8, cameraYaw=0, cameraPitch=-45, cameraTargetPosition=target)
-    gripper1 = Robotiq2F85()
-    robot1 = UR3e(simulate_real_time=True)
+    gripper1 = WSG50()
+    robot1 = UR3e(simulate_real_time=True,gripper=gripper1,eef_start_pose=np.array([0.2,-0.1,0.3,1,0,0,0]))
 
 
     gripper2 = Robotiq2F85()
     robot2 = UR3e(simulate_real_time=True,robot_base_position=[0.5,0.0,0.0],gripper=gripper2)
 
-    # for i in range(p.getNumJoints(gripper.gripper_id)):
-    #     print(p.getJointInfo(gripper.gripper_id, i))
-    gripper1.reset(robot1.get_eef_pose())
-    gripper1.attach_with_constraint_to_robot(robot1.robot_id,robot1.eef_id)
-    
-    # gripper2.reset(robot2.get_eef_pose())
-    # gripper2.attach_with_constraint_to_robot(robot2.robot_id,robot2.eef_id)
+    for i in range(p.getNumJoints(gripper1.gripper_id)):
+        print(p.getJointInfo(gripper1.gripper_id, i))
+
 
     gripper1.close_gripper()
     gripper2.close_gripper()
 
-    robot1.movep([0.2,-0.2,0.2,1,0,0,0],speed=0.001)
+    robot1.movep([0.2,-0.2,0.0,1,0,0,0],speed=0.001)
     gripper1.open_gripper()
+    gripper1.close_gripper()
 
     robot2.movep([0.2,-0.2,0.2,1,0,0,0],speed=0.001) #TODO: fix bug w/ base_position for robots.
     gripper2.open_gripper()
+    gripper2.close_gripper()
     time.sleep(100)
     p.disconnect()
